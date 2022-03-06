a21a40d26dbeb4fef9d46517ee92ac65b7249f94
Ricky Surya Adiputra <rickysuryaa10@gmail.com>
2022-03-06 17:50:43 +0700

    Added html and css

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 from markupsafe import escape
 import uuid
 
@@ -6,28 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
-
-@app.route("/<name>")
-def test(name):
-    return f"Test, {escape(name)}"
-
-@app.route("/post/<int:post_id>")
-def post(post_id): 
-    return f'Post {post_id}'
-
-@app.route("/home")
-def home():
-    uuidOne = uuid.uuid1()
-    return f'{uuidOne}'
-
-@app.route("/path/<path:subpath>")
-def show_subpath(subpath):
-    return f'The subpath you just entered is {escape(subpath)}'
-
-@app.route("/nothing/")
-def about():
-    return "Nothing here"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
